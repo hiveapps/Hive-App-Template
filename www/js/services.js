f827cdf5e9baaf576228428d7b166c5aa96f2019
atans2468 <austintans7@gmail.com>
2016-02-21 14:11:54 -0800

    adding in firebase 'Message' factory

--- www/js/services.js
+++ www/js/services.js
@@ -13,19 +13,25 @@ hive.factory('postService', function($firebaseArray) {
 	return postService;
 });
 
-hive.factory('Message', [function() {
-
-	var messages = [{'name':'Pippo','text':'Hello'},
-					{'name':'Pluto','text':'Hello'},
-					{'name':'Pippo','text':'how are you ?'},
-					{'name':'Pluto','text':'fine thanks'},
-					{'name':'Pippo','text':'Bye'},
-					{'name':'Pluto','text':'Bye'}];
-
-	var Message = {
-		all: messages
-	};
-
-	return Message;
-
-}]);
+hive.factory('Message', ['$firebase',
+	function($firebase) {
+		var ref = new Firebase('https://10minute.firebaseio.com/');
+		var messages = $firebase(ref.child('messages')).$asArray();
+ 
+		var Message = {
+			all: messages,
+			create: function (message) {
+				return messages.$add(message);
+			},
+			get: function (messageId) {
+				return $firebase(ref.child('messages').child(messageId)).$asObject();
+			},
+			delete: function (message) {
+				return messages.$remove(message);
+			}
+		};
+ 
+		return Message;
+ 
+	}
+	]);
